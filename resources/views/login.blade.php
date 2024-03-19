b66051b6cc9f07e6c447e1fa48adfd3400584b30
hazel-montoya <hazel.montoya2025@poma.superate.org.sv>
2024-03-19 15:45:56 -0600

    submit 3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-<div class="text-5x1 text-green-600 uppercase">
-  <h1 class="text-9xl text-fuchsia-700">Log In</h1>
+<div class="text-5x1 text-green-800 uppercase">
+  <h1 class="text-9xl text-fuchsia-300">Log In</h1>
     <form action="/login" method="POST" class="flex flex-col justify-center my-12">
        @csrf
         <div>
@@ -10,6 +10,6 @@
             <x-input titulo="Password" type="password" nombre="password"/>
         </div>
 
-        <button type="submit" class="bg-lime-200 text-white font-semibold px-5py-2 rounded-2x">Log In</button>
+        <button type="submit" class="bg-purple-900 text-white font-semibold px-5py-2 rounded-2x text-center">Log In</button>
     </form>
 </x-layout>
